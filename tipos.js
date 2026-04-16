abaaf27d2027f1e58f47267a54e8171a23f9da8c
coly1090 <ferreira.emyly@escola.pr.gov.br>
2026-04-15 22:00:55 -0300

    Add files via upload

---
+++ tipos.js
@@ -0,0 +1,32 @@
+// boolean 
+
+let ativo = true;
+
+console.log(typeof ativo);
+console.log(ativo);
+
+// number
+let idade = 25;
+let pi = 3.14;
+console.log(typeof idade);
+console.log(typeof pi);
+
+//string e template literals 
+let nome = "maria";
+let mensagem = "olá, ${nome}!";
+console.log(mensagem);
+
+// undefined
+let valorindefinido;
+console.log(typeof valorindefinido);
+console.log(valorindefinido);
+
+//null
+let valornulo = null;
+console.log(typeof valornulo);
+console.log(valornulo);
+
+//bigint
+let numerogrande = 9007199254740991n;
+console.log(typeof numerogrande);
+console.log(numerogrande);
